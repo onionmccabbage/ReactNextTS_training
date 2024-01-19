bbf882d04df054ca61c6a8d792e43a74dc570556
onionmccabbage <onionmccabbage@live.com>
2024-01-19 14:06:33 +0000

    resources

---
+++ Gregor.js
@@ -0,0 +1,9 @@
+// nb cannot export default const
+export const gregor = {
+    name: 'Gregorio Y. Zara',
+    imgSrc: "https://i.imgur.com/7vQD0fPs.jpg",
+    theme: {
+      backgroundColor: 'black', // NB inline css camel
+      color: 'pink'
+    }
+  };

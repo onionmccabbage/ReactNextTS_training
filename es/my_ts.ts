450f47d8594477ccc0a2676de3c0a5b082d99a48
onionmccabbage <onionmccabbage@live.com>
2024-01-23 10:49:35 +0000

    ES and TS inc scope

--- es/my_ts.ts
+++ es/my_ts.ts
@@ -5,39 +5,41 @@
 
 // this line infers the type
 let a = 1 // instead of let a:number
-
-// you can declare your own types
-type MyType = Snap | boolean | number | 'an error type'
-let mt:MyType = true // ok but we must type this
-// there will be NO TYPE CHECKING at runtime
-mt = 33
-// a type is a constraint, the data can ONLY be one of these types
-type Kingdom  = 'Bacteria' | 'Protozoa' | 'Chromista'
-let k:Kingdom = 'Protozoa'
-type Arbitrary = 'literaly this' | 10 | false // any mix of types
-let r:Arbitrary = false
-
-const fnB = (n:number|string)=>{
-    // here we see a type guard
-    if (typeof(n)=='number'){
-    return n**2
-    } else if (typeof(n)=='string'){
-        return n
-    }
+{ // modern ES has block scope
+    // you can declare your own types
+    type MyType = Snap | boolean | number | 'an error type'
+    let mt: MyType = true // ok but we must type this
+    // there will be NO TYPE CHECKING at runtime
+    mt = 33
+    // a type is a constraint, the data can ONLY be one of these types
+    type Kingdom = 'Bacteria' | 'Protozoa' | 'Chromista'
+    let k: Kingdom = 'Protozoa'
+    type Arbitrary = 'literaly this' | 10 | false // any mix of types
+    let r: Arbitrary = false
 }
+{
+    const fnB = (n: number | string) => {
+        // here we see a type guard
+        if (typeof (n) == 'number') {
+            return n ** 2
+        } else if (typeof (n) == 'string') {
+            return n
+        }
+    }
 
-let result = fnB('3')
+    let result = fnB('3')
+}
 
 // interfaces (ts) and classes (es)
 export interface Photo {
-    albumId      :  number
-    id           :  number
-    title        :  string
-    url          :  string
-    thumbnailUrl :  string
-    ooblywoobly? :  object // an optional member
+    albumId: number
+    id: number
+    title: string
+    url: string
+    thumbnailUrl: string
+    ooblywoobly?: object // an optional member
     // we can declare methods
-    pretty? : (x:boolean)=>{} // we could declare a return type, but no need
+    pretty?: (x: boolean) => {} // we could declare a return type, but no need
 }
 class Snap implements Photo {
     albumId: number
@@ -46,33 +48,33 @@ class Snap implements Photo {
     url: string
     thumbnailUrl: string
     // we can optionally declare a constructor
-    constructor(snap:Photo){
+    constructor(snap: Photo) {
         this.albumId = snap.albumId
         this.id = snap.id
         this.title = snap.title
         this.url = snap.url
         this.thumbnailUrl = snap.thumbnailUrl
     }
-    pretty ():string {
+    pretty(): string {
         return `${this.title}: ${this.url}`
     };
 }
 
 // enum
 const enum CategoryFlags {
-    None    = 0,
-    Music   = 1,
-    News    = 2,
-    Drama   = 4,
-    Comedy  = 8,
+    None = 0,
+    Music = 1,
+    News = 2,
+    Drama = 4,
+    Comedy = 8,
     Fiction = 16
 }
 // combinations will always be unique
 const MusicalComedy = CategoryFlags.Music | CategoryFlags.Comedy
 
-const hasMusic   = (MusicalComedy & CategoryFlags.Music)  // 1001 & 0001
-const hasComedy  = (MusicalComedy & CategoryFlags.Comedy) // 1001 & 1000
-const hasDrama   = (MusicalComedy & CategoryFlags.Drama)  // 1001 & 0100
+const hasMusic = (MusicalComedy & CategoryFlags.Music)  // 1001 & 0001
+const hasComedy = (MusicalComedy & CategoryFlags.Comedy) // 1001 & 1000
+const hasDrama = (MusicalComedy & CategoryFlags.Drama)  // 1001 & 0100
 
 console.log(hasMusic, hasComedy, hasDrama)
 

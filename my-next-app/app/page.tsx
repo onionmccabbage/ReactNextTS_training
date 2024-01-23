d5e590e4887b279f112e5b5ea3a83ee5f0c31846
onionmccabbage <onionmccabbage@live.com>
2024-01-23 16:29:49 +0000

    passing data around (end of day 2)

--- my-next-app/app/page.tsx
+++ my-next-app/app/page.tsx
@@ -1,10 +1,17 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import BrownComp from "./BrownComp";
 
 export default function Home() {
+  const doStuff = ()=>{
+    alert(`Stuff happened`)
+  }
+  const xval = 1
+  const yval = true
    return (
    <>
     <h3>My React/Next App</h3>
+    <BrownComp x={xval} y={yval} action={doStuff} />
    </>
   );
 }

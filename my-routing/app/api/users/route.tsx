5a40519d2430ff4d67b2b3c14c748499383b40bd
onionmccabbage <onionmccabbage@live.com>
2024-01-25 13:42:32 +0000

    consuming teh Next.js API in a page

--- my-routing/app/api/users/route.tsx
+++ my-routing/app/api/users/route.tsx
@@ -1,13 +1,13 @@
 import { NextResponse } from "next/server";
 
 // remember - every page request is a 'GET'
-export async function GET(){
+export async function GET() {
     // it is a really good idea to ALWAYS send a 'data' json top-level
     return NextResponse.json(
         {
             // any other page can then 'fetch' this data
-            "data":[{"name":"Agnes"},{"name":"Bertha"},{"name":"Cerise"}]
+            "data": [{ "name": "Freda" }, { "name": "Bertha" }, { "name": "Cerise" }]
         }
-        )
+    )
 
 }
--- my-routing/app/users/page.tsx
+++ my-routing/app/users/page.tsx
@@ -1,9 +1,24 @@
 // layout is NOT essential
-export default function Users(){
 
+// code to fetch data from the (next.js) api
+async function getAllUsers(){
+    const data = await fetch(`http://localhost:3000/api/users`)
+    if(!data.ok){
+        throw new Error('failed to fetch')
+    }
+    return data.json() // just return the json part of the response
+}
+// sunce the API data-fetching is async, our page function must be async
+export default async function Users(){
+    const data = await getAllUsers()
     return (
         <section>
             <h3>Users page</h3>
+            {/* a very tidy way to see the returned data from ANY API */}
+            <pre>{JSON.stringify(data)}</pre>
+            <p>{data.data[0].name}</p>
+            <p>{data.data[1].name}</p>
+            <p>{data.data[2].name}</p>
         </section>
     )
 }
